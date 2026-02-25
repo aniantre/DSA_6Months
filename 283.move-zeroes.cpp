b438ba8dda881c9ef01a8c041b9158e22bdd0210
Aniket Antre <aniantre@gmail.com>
2026-02-25 23:42:34 +0000

    move zeroes to end of array by not diturbing the sequence of non zero element

---
+++ 283.move-zeroes.cpp
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode id=283 lang=cpp
+ *
+ * [283] Move Zeroes
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    void moveZeroes(vector<int> &nums)
+    {
+        int j = 0; // write pointer
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (nums[i] != 0)
+            {
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+
+        while (j < nums.size())
+        {
+            nums[j] = 0;
+            j++;
+        }
+    }
+};
+// @lc code=end
